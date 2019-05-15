5d9853ec51345e997f9904ebe9bebff547acdadf
The Original G <33421040+OguzDastan@users.noreply.github.com>
2019-05-15 04:20:42 +0200

    some new shit

---
+++ ConsumerUWP/ViewModels/OverviewVM.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using ConsumerUWP.Annotations;
+using GalaSoft.MvvmLight.Command;
+using Models;
+
+namespace ConsumerUWP.ViewModels
+{
+    class OverviewVM : INotifyPropertyChanged
+    {
+
+        private string _controller;
+
+        // her huskes den valgte bog :-)
+        private OverviewList _selectedBook;
+
+        // Her gemmes alle bøger - observable coolection betyder at den kan notificere GUI om ændringer
+        private ObservableCollection<OverviewList> _overviewlist = new ObservableCollection<OverviewList>();
+
+        public OverviewVM()
+        {
+            _overviewlist.Add(new OverviewList("Etiket Operatør", "15-05-2019 / 7.30"));
+            _overviewlist.Add(new OverviewList("Tappe Operatør", "15-05-2019 / 11.30"));
+            _overviewlist.Add(new OverviewList("Trykkontrol", "15-05-2019 / 14.30"));
+            _overviewlist.Add(new OverviewList("Færdigvare Kontrol", "15-05-2019 / 18.30"));
+
+            LoanBookCommand = new RelayCommand(
+                addLoanertoSelectedBook);
+
+        }
+
+        public OverviewList SelectedBook
+        {
+            get { return _selectedBook; }
+            set
+            {
+                _selectedBook = value;
+                OnPropertyChanged(); // fortæller XAML at property er opdateret
+            }
+        }
+
+        // XAML binding til ListView
+        public ObservableCollection<OverviewList> OverviewLists
+        {
+            get { return _overviewlist; }
+            set { _overviewlist = value; }
+        }
+
+        public string Controller
+        {
+            get { return _controller; }
+            set
+            {
+                _controller = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RelayCommand LoanBookCommand
+        {
+            get;
+            private set;
+        }
+
+        private void addLoanertoSelectedBook()
+        {
+            Debug.WriteLine("add loaner");
+            //Debug.WriteLine("Loaner: " + Loaner);
+            //Debug.WriteLine("SelectedBook_: "+ SelectedBook.Title);
+            Debug.WriteLine(SelectedBook);
+            SelectedBook.Controller = Controller;
+            //OnPropertyChanged("SelectedBook");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Models/OverviewList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class OverviewList : INotifyPropertyChanged
+    {
+        private string _arktype;
+        private string _title;
+        private string _controller;
+
+        public OverviewList(string arktype, string title)
+        {
+            _arktype = arktype;
+            _title = title;
+        }
+
+        public string ArkType
+        {
+            get { return _arktype; }
+            set { _arktype = value; }
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public string Controller
+        {
+            get { return _controller; }
+            set
+            {
+                _controller = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("title:");
+            sb.Append(Title);
+            sb.Append("\n");
+            sb.Append("kontrol:");
+            sb.Append(Controller);
+            return sb.ToString();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
