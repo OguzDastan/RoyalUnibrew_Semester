b6bf841ac91d9109cd542554085076d6b5a5ef6a
MSI\Mathias <math662h@edu.easj.dk>
2019-05-20 13:59:10 +0200

    Activity SQL

---
+++ RestService/Controllers/ActivityController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RestService.Managers;
+using Models;
+
+namespace RestService.Controllers
+{
+    public class ActivityController : ApiController
+    {
+
+        ActivitiesManager activitiesManager = new ActivitiesManager();
+        
+
+        // GET: api/Activity
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Activity/5
+        [Route ("api/Activity/{id}")]
+        public Activity Get(int id)
+        {
+            return activitiesManager.Get(id);
+        }
+
+        // POST: api/Activity
+        public bool Post([FromBody]Activity value)
+        {
+            return activitiesManager.Post(value);
+        }
+
+        // PUT: api/Activity/5 //Problemer med PUT
+        [Route("api/Activity/{id}")]
+        public bool Put(int id, [FromBody]Activity value)
+        {
+            return activitiesManager.Put(id, value);
+        }
+
+        // DELETE: api/Activity/5 // prob
+        [Route("api/Activity/{id}")]
+        public bool Delete(int id)
+        {
+            return activitiesManager.Delete(id);
+        }
+    }
+}
---
+++ RestService/Managers/ActivitiesManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using Activity = Models.Activity;
+
+namespace RestService.Managers
+{
+    public class ActivitiesManager
+    {
+        //queries
+        private const string GET_ONE = "SELECT * FROM Activities WHERE ActivityID = @ActivityID";
+        private const string INSERT = "INSERT INTO Activities values (@ActivityName)";
+        private const string UPDATE = "UPDATE Activities" + 
+                                      "SET ActivityName = @ActivityName" +
+                                      "WHERE ActivityID = @ActivityID";
+        private const string DELETE = "DELETE FROM Activities WHERE ActivityID = @ID";
+
+        //Look up activity by ActivityID
+        public Activity Get(int ActivityID)
+        {
+            //create empty activity object
+            Activity a = null;
+
+            //query database 
+            using (SqlCommand cmd = new SqlCommand(GET_ONE, SQLConnectionSingleton.Instance.DbConnection))
+            {
+                //binding of relevent DB parameters
+                cmd.Parameters.AddWithValue("@ActivityID", ActivityID);
+
+                //Reader to handle the result
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                //while there's a result
+                while (reader.Read())
+                {
+                    a = new Activity()
+                    {
+                        ActivityID = reader.GetInt32(0),
+                        ActivityName = reader.GetString(1).ToString()                                         
+                    };
+                }
+                //the IO stream of data, coming from database is closed
+                reader.Close();
+            }
+            //Return the activity from database, or "null" if no user what Activity = ActivityID
+            return a;
+        }
+
+        public bool Post(Activity activity)
+        {
+            SqlCommand cmd = new SqlCommand(INSERT, SQLConnectionSingleton.Instance.DbConnection);
+            cmd.Parameters.AddWithValue("@ActivityName", activity.ActivityName);
+            int rowsAffected = cmd.ExecuteNonQuery();
+
+            return rowsAffected == 1;
+        }
+
+        public bool Put(int id, Activity activity)
+        {
+            SqlCommand cmd = new SqlCommand(UPDATE, SQLConnectionSingleton.Instance.DbConnection);
+            cmd.Parameters.AddWithValue("@ActivityName", activity.ActivityName);
+            cmd.Parameters.AddWithValue("@ActivityID", id);
+
+            int noOfRows = cmd.ExecuteNonQuery();
+            return noOfRows == 1;
+        }
+
+        public bool Delete(int id)
+        {
+            SqlCommand cmd = new SqlCommand(DELETE, SQLConnectionSingleton.Instance.DbConnection);
+            cmd.Parameters.AddWithValue("@ID", id);
+
+            int noOfRows = cmd.ExecuteNonQuery();
+
+            return noOfRows == 1;
+
+        }
+
+    }
+
+}
