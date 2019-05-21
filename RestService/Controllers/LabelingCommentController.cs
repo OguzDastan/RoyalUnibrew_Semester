db17adb251b906a6be206538ed4fa7e8dff055ff
mathias brink <mathiasbrink@live.dk>
2019-05-21 11:41:36 +0200

    Added LabelingCommentManager

---
+++ RestService/Controllers/LabelingCommentController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RestService.Controllers
+{
+    public class LabelingCommentController : ApiController
+    {
+        // GET: api/LabelingComment
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/LabelingComment/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/LabelingComment
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/LabelingComment/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/LabelingComment/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
