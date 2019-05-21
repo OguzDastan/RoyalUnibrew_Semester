4ed3b0c369f0267cdd62ba88859debee1ea770c8
mrkiiro <kiiro.crafter@gmail.com>
2019-05-22 00:45:44 +0200

    Redoing parts of PalletCheckManager

--- RestService/Managers/PalletCheckManager.cs
+++ RestService/Managers/PalletCheckManager.cs
@@ -11,12 +11,16 @@ namespace RestService.Managers
     {
         //queries
         private const string GET_ONE = "SELECT * FROM PalleCheck WHERE ProcessOrderNR = @ProcessOrderNR AND TimeOfTest = @TimeOfTest";
-        private const string INSERT = "INSERT INTO PalleCheck values (@ProcessOrderNR)";
-
+        private const string GET_BY_PROCESSORDER = "SELECT * FROM PalleCheck WHERE ProcessOrderNR = @ProcessOrderNR";
+        private const string GET_ALL = "SELECT * FROM PalleCheck";
+        private const string INSERT = "INSERT INTO PalleCheck values (@ProcessOrderNR, @TimeOfTest, @Pallet, @WorkerID)";
         private const string UPDATE = "UPDATE PalleCheck " +
                                       "SET Pallet = @Pallet " +
+                                      "TimeOfTest = @TimeOfTest " +
+                                      "WokerID = @WorkerID "
                                       "WHERE ProcessOrderNR = @ProcessOrderNR ";
-        private const string DELETE = "DELETE FROM PalleCheck WHERE ProcessOrderNR = @ID ";
+        private const string DELETE_BY_PROCESSNR = "DELETE FROM PalleCheck WHERE ProcessOrderNR = @ID ";
+        private const string DELETE_ONE = "SELECT FROM PalleCheck WHERE ProcessOrderNR = @ID AND TimeOfTest = @TimeOfTest";
 
         public PalletCheck Get(int ProcessOrderNR, TimeSpan TimeOfTest)
         {
@@ -40,24 +44,73 @@ namespace RestService.Managers
                         WorkerID = reader.GetInt32(3)
                     };
                 }
-                reader.Close();    
+                reader.Close();
             }
             return pallet;
         }
+        public List<PalletCheck> Get(int ProcessOrderNR)
+        {
+            List<PalletCheck> entries = new List<PalletCheck>();
 
+            using (SqlCommand cmd = new SqlCommand(GET_BY_PROCESSORDER, SQLConnectionSingleton.Instance.DbConnection))
+            {
+                cmd.Parameters.AddWithValue("@ProcessOrderNR", ProcessOrderNR);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    entries.Add(new PalletCheck()
+                    {
+                        ProcessOrderNR = reader.GetInt32(0),
+                        TimeOfTest = reader.GetTimeSpan(1),
+                        Pallet = reader.GetString(2),
+                        WorkerID = reader.GetInt32(3)
+                    });
+                }
+                reader.Close();
+            }
+            return entries;
+        }
+        public List<PalletCheck> Get()
+        {
+            List<PalletCheck> entries = new List<PalletCheck>();
+
+            using (SqlCommand cmd = new SqlCommand(GET_ALL, SQLConnectionSingleton.Instance.DbConnection))
+            {
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    entries.Add(new PalletCheck()
+                    {
+                        ProcessOrderNR = reader.GetInt32(0),
+                        TimeOfTest = reader.GetTimeSpan(1),
+                        Pallet = reader.GetString(2),
+                        WorkerID = reader.GetInt32(3)
+                    });
+                }
+                reader.Close();
+            }
+            return entries;
+        }
         public bool Post(PalletCheck palletCheck)
         {
             SqlCommand cmd = new SqlCommand(INSERT, SQLConnectionSingleton.Instance.DbConnection);
             cmd.Parameters.AddWithValue("@ProcessOrderNR", palletCheck.ProcessOrderNR);
+            cmd.Parameters.AddWithValue("@TimeOfTest", palletCheck.TimeOfTest);
+            cmd.Parameters.AddWithValue("@Pallet", palletCheck.Pallet);
+            cmd.Parameters.AddWithValue("@WorkerID", palletCheck.WorkerID);
             int rowsAffected = cmd.ExecuteNonQuery();
 
             return rowsAffected == 1;
         }
-
         public bool Put(int id, PalletCheck palletCheck)
         {
             SqlCommand cmd = new SqlCommand(UPDATE, SQLConnectionSingleton.Instance.DbConnection);
             cmd.Parameters.AddWithValue("@Pallet ", palletCheck.Pallet);
+            cmd.Parameters.AddWithValue("@TimeOfTest", palletCheck.TimeOfTest);
+            cmd.Parameters.AddWithValue("@WorkerID", palletCheck.WorkerID);
             cmd.Parameters.AddWithValue("@ProcessOrderNR ", id);
 
             int rowsAffected = cmd.ExecuteNonQuery();
@@ -67,12 +120,21 @@ namespace RestService.Managers
 
         public bool Delete(int id)
         {
-            SqlCommand cmd = new SqlCommand(DELETE, SQLConnectionSingleton.Instance.DbConnection);
+            SqlCommand cmd = new SqlCommand(DELETE_BY_PROCESSNR, SQLConnectionSingleton.Instance.DbConnection);
             cmd.Parameters.AddWithValue("@ID ", id);
 
             int rowsAffected = cmd.ExecuteNonQuery();
 
-            return rowsAffected == 1;
+            return rowsAffected > 0;
+        }
+        public bool Delete(int id, TimeSpan TimeOfTest)
+        {
+            SqlCommand cmd = new SqlCommand(DELETE_BY_PROCESSNR, SQLConnectionSingleton.Instance.DbConnection);
+            cmd.Parameters.AddWithValue("@ID ", id);
+
+            int rowsAffected = cmd.ExecuteNonQuery();
+
+            return rowsAffected > 0;
         }
     }
 }
